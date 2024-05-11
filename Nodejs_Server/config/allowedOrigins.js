b7809edc50b72f452714c9c956a60dd2c3e24765
VKev <huynhkhang7452@gmail.com>
2024-05-11 16:19:02 +0700

    up

--- Nodejs_Server/config/allowedOrigins.js
+++ Nodejs_Server/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:2000",
-  "http://www.vkevhandwritingrecognition.com",
-  "http://vkevhandwritingrecognition.com",
+  "http://www.vkev-handwriting-recognition-0ee575f5577e.herokuapp.com",
+  "https://vkev-handwriting-recognition-0ee575f5577e.herokuapp.com",
 ];
 
 module.exports = allowedOrigins;
--- Nodejs_Server/controller/guessController.js
+++ Nodejs_Server/controller/guessController.js
@@ -4,9 +4,12 @@ const axios = require("axios");
 const guess = asyncHandler(async (req, res) => {
   try {
     const matrix = req.body;
-    const response = await axios.post("http://localhost:5000/predict", {
-      matrix,
-    });
+    const response = await axios.post(
+      "https://flask-handwriting-process-7dc3af01bcd7.herokuapp.com/predict",
+      {
+        matrix,
+      }
+    );
     const prediction = response.data.prediction;
     res.json({ prediction });
   } catch (error) {
